65288b38a3947861d7b7aac27b7bde673ea360df
Elg Dev <elgthedev@gmail.com>
2022-10-31 18:16:59 -0300

    refactor: remove transport creatures on recall, player use only so far

--- TeleportEverything/CompatibilitySkyheim.cs
+++ TeleportEverything/CompatibilitySkyheim.cs
@@ -1,7 +1,6 @@
 ﻿using BepInEx.Bootstrap;
 using HarmonyLib;
 using System;
-using UnityEngine;
 
 namespace TeleportEverything
 {
@@ -26,56 +25,17 @@ namespace TeleportEverything
                         new HarmonyMethod(AccessTools.Method(typeof(Plugin), nameof(SkyheimBlinkPrefix))),
                         new HarmonyMethod(AccessTools.Method(typeof(Plugin), nameof(SkyheimBlinkPostfix)))
                     );
-                    TeleportEverythingLogger.LogInfo("Skyheim blink settings applied");
+                    TeleportEverythingLogger.LogInfo("Skyheim settings applied");
                 }
                 catch (Exception ex)
                 {
-                    TeleportEverythingLogger.LogInfo($"Failed to apply blink settings to skyheim: {ex.Message}");
-                }
-            }
-
-            var recallClass = Type.GetType("SkyheimAttackRecall, skyheim");
-            if (recallClass != null)
-            {
-                try
-                {
-                    var method = AccessTools.Method(recallClass, "OnAttackTrigger");
-                    _harmony.Patch(method,
-                        null,
-                        new HarmonyMethod(AccessTools.Method(typeof(Plugin), nameof(SkyheimRecallPrefix)))
-                    );
-                    TeleportEverythingLogger.LogInfo("Skyheim recall settings applied");
-                }
-                catch (Exception ex)
-                {
-                    TeleportEverythingLogger.LogInfo($"Failed to apply recall settings to skyheim: {ex.Message}");
+                    TeleportEverythingLogger.LogInfo($"Failed to apply settings to skyheim: {ex.Message}");
                 }
             }
         }
 
         private static void SkyheimBlinkPrefix() => isSkyheimBlink = true;
         private static void SkyheimBlinkPostfix() => isSkyheimBlink = false;
-
-        private static void SkyheimRecallPrefix(Player player)
-        {
-            if (!EnableMod.Value) return;
-
-            player.m_teleportTargetPos = Game.instance.GetPlayerProfile().GetHomePoint();
-
-            SetIncludeMode();
-            GetCreatures();
-
-            if (Enemies.Count > 0 && TeleportMode.Value.Contains("Take"))
-            {
-                TeleportCreatures(player, Enemies, true);
-            }
-
-            TeleportEverythingLogger.LogInfo($"Allies: {Allies.Count} and Transport: {TransportAllies}");
-            if (Allies.Count > 0 && TransportAllies)
-            {
-                TeleportCreatures(player, Allies);
-            }
-        }
 
         [HarmonyPatch(typeof(Game), nameof(Game._RequestRespawn))]
         public class _RequestRespawn_Patch
